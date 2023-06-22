fc90bded8b3deb1f3088e89cb1ed69a57677b677
jholloway <jholloway@griddynamics.com>
2023-06-22 16:27:40 -0500

    create CatalogSection component

---
+++ src/components/CatalogSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="parent-container catalog-parent">
+    <div class="content-container catalog-container">
+      <div class="catalog-option">
+        <img src="../assets/pre-arranged.jpg" alt="" class="catalog-pic" />
+        <p class="catalog-text">Shop for pre-arranged bouquets</p>
+        <button class="shop-now-btn">Shop Now</button>
+      </div>
+      <div class="catalog-option">
+        <img src="../assets/custom-bouquet.jpg" alt="" class="catalog-pic" />
+        <p class="catalog-text">
+          Create your <br />
+          own bouquet
+        </p>
+        <button class="shop-now-btn">Shop Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
