97e0987e511b5817ec16e60fda246ff09ff57f26
jholloway <jholloway@griddynamics.com>
2023-06-22 15:08:06 -0500

    create HomeBanner component and add images for the site

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-banner">
+    <NavigationBar :isHome="true" />
+    <h1 class="home-banner-text">Show them love with Bloom</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavigationBar from "./NavigationBar.vue";
+</script>
+
+<style scoped>
+.home-banner {
+  background: url("../assets/home-banner-img.jpg"),
+    lightgray 50% / cover no-repeat;
+}
+
+.home-banner-text {
+  color: #000;
+  text-align: center;
+  font-size: 64px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+}
+</style>
