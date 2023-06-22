cd0775176aa0dc500a5d6de32ea28574e3d3b41f
jholloway <jholloway@griddynamics.com>
2023-06-22 16:43:54 -0500

    add styling to CatalogSection

--- src/components/CatalogSection.vue
+++ src/components/CatalogSection.vue
@@ -3,7 +3,10 @@
     <div class="content-container catalog-container">
       <div class="catalog-option">
         <img src="../assets/pre-arranged.jpg" alt="" class="catalog-pic" />
-        <p class="catalog-text">Shop for pre-arranged bouquets</p>
+        <p class="catalog-text">
+          Shop for pre-arranged <br />
+          bouquets
+        </p>
         <button class="shop-now-btn">Shop Now</button>
       </div>
       <div class="catalog-option">
@@ -20,4 +23,48 @@
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.catalog-parent {
+  background: #dda15e;
+}
+
+.catalog-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 100px 0;
+}
+
+.catalog-option {
+  width: 538px;
+  display: flex;
+  flex-direction: column;
+  border: 2px solid #283618;
+  border-radius: 8px;
+  padding-bottom: 30px;
+}
+
+.catalog-pic {
+  width: 100%;
+  height: 596px;
+  border-radius: 8px;
+}
+
+.catalog-text,
+.shop-now-btn {
+  color: #283618;
+  text-align: center;
+  font-size: 40px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  margin-top: 35px;
+}
+
+.shop-now-btn {
+  width: 294px;
+  height: 106px;
+  border-radius: 50px;
+  border: 4px solid #283618;
+  background: transparent;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
   <div>
     <HomeBanner />
     <AboutSection />
+    <CatalogSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import HomeBanner from "@/components/HomeBanner.vue";
 import AboutSection from "@/components/AboutSection.vue";
+import CatalogSection from "@/components/CatalogSection.vue";
 </script>
 
 <style scoped></style>
