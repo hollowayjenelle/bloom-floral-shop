92b17376726bae185d16fde981f31b8cb8894c7b
jholloway <jholloway@griddynamics.com>
2023-06-22 15:00:37 -0500

    create NavigationBar component

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="navigation-bar">
+    <h1 class="company-name">Bloom</h1>
+    <div class="navigation-links">
+      <a href="">About</a>
+      <a href="">Catalog</a>
+      <a href="">Reviews</a>
+      <a href="">Contact</a>
+    </div>
+    <button v-if="isHome" class="shop-btn">Shop Now</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  isHome: Boolean,
+});
+</script>
+
+<style scoped>
+.navigation-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.company-name {
+  color: #000;
+  font-size: 40px;
+  font-family: "Crimson Text", serif;
+  letter-spacing: 2.4px;
+}
+
+.navigation-links {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 524px;
+}
+
+.navigation-links a {
+  color: #000;
+  font-size: 24px;
+  font-family: "Poppins", sans-serif;
+}
+
+.shop-btn {
+  width: 178px;
+  height: 58px;
+  border-radius: 50px;
+  background: #dda15e;
+  color: white;
+}
+</style>
